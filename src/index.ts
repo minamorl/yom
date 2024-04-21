617d4c90e952cbfa6fc4cd430b5dba031f66c16c
minamorl <minamorl@users.noreply.github.com>
2024-04-21 23:45:27 +0900

    yom init

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { command, run, string, positional } from 'cmd-ts';
+import puppeteer from 'puppeteer'
+
+const handler = async ({ url }: { url: string }) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    // Get all the text content from the page
+    const pageContent = await page.evaluate(() => {
+        return document.body.innerText;
+    });
+    await browser.close();
+    console.log(pageContent);
+
+
+}
+
+const main = () => {
+    
+    const app = command({
+        name: 'yom',
+        args: {
+            url: positional({ type: string }),
+        },
+        handler
+    });
+    run(app, process.argv.slice(2))
+}
+main()
